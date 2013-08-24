abcd3a605ed24f9799252f5b13e2b748bd8c341c
George Redinger <george@georgeredinger.com>
2013-08-24 14:25:13 -0700

    fix some constants

--- PulseCounter/PulseCounter.ino
+++ PulseCounter/PulseCounter.ino
@@ -1,16 +1,11 @@
 const int hardwareCounterPin = 5;   // input pin fixed to internal Timer
-
-
 const int samplePeriod = 1000;   // the sample period in milliseconds
 
-#define ledPin 13
 
 void setup()
 {
   noInterrupts(); 
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, digitalRead(ledPin) ^ 1);   // toggle LED pin
-  
+
   TCCR1A = 0;
   TCCR1B = 0;
   TCNT1  = 0;
@@ -25,25 +20,26 @@ void setup()
   bitSet(TCCR1B ,CS12);  // Counter Clock source is external pin
   bitSet(TCCR1B ,CS11);  // Clock on rising edge  
   TCCR1A=0;        // reset timer/counter control register A
-
+  interrupts();  
+  
   Serial.begin(115200);
   Serial.println("Pulse Counter");
-    Serial.println("Counts/hour, total counts since restart");
-  interrupts();  
+  Serial.println("Hours,  Counts/hour, total counts since restart");
+
  
 }  
 
-
+//called when timer reaches the compare match setting
+//won't happen as long as it's being set to 0 every second in the main loop
 ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
 {
    Serial.println("Compare match");
-   digitalWrite(ledPin, digitalRead(ledPin) ^ 1);   // toggle LED pin
-   count += 100;
-     
 }
 
 
 // every 65536 counts
+//won't happen as long as it's being set to 0 in the loop() every second
+
 ISR(TIMER1_OVF_vect)          
 {
    Serial.println("Timer Overflow");
@@ -54,16 +50,26 @@ void loop()
   unsigned long start;
   unsigned long elapsed;
   unsigned long total=0L;
-  
+  unsigned long counts_per_hour;
+  float hours;
+  const unsigned long millis_per_hour = (1000*60*60);
   start = millis();
+  
   for(;;) {
   // start the counting
    delay(samplePeriod);
   
-  total +=TCNT1
+  total +=TCNT1;
   TCNT1 = 0;
+  
   elapsed = millis()-start;
-  Serial.print((total/elapsed)/(1000*60*60));
+  
+  hours = ((float)(millis()-start))/(3600000.0F);//decimal hours since start
+  counts_per_hour = total/hours;
+  
+  Serial.print(hours);
+  Serial.print(",");
+  Serial.print(counts_per_hour);
   Serial.print(",");
   Serial.println(total);
   
