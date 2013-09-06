bccb889644fce6ce36e3e996af91b2feb0ef0fb7
George Redinger <george@georgeredinger.com>
2013-09-06 14:03:43 -0700

    pinchange counter on analog 0, drive LCD with minutes,counts/minute, total counts

--- PulseCounter/PulseCounter.ino
+++ PulseCounter/PulseCounter.ino
@@ -1,92 +1,100 @@
+#include <LiquidCrystal.h>
+//#include <LCDKeypad.h>
+
 const int hardwareCounterPin = 5;   // input pin fixed to internal Timer
 const int samplePeriod = 1000;   // the sample period in milliseconds
 const int RedLED = 13;
 const int GreenLED = 12;
+unsigned long count = 0L;
+unsigned long lastcount = 0;
+unsigned long lastminutes = 0;
+
+LiquidCrystal lcd( 8, 9, 4, 5, 6, 7 );
+volatile const uint8_t sensPin = 0;
+volatile uint8_t analog_reference = DEFAULT;
+
+volatile unsigned long IRQ8PrevTime;
+volatile int prevVal = 0;
+volatile uint8_t irqFlag = 0;
 
 void setup()
 {
   noInterrupts(); 
 
-  TCCR1A = 0;
-  TCCR1B = 0;
-  TCNT1  = 0;
-
-//enable this block if you want to an interupt every 100 counts and counter reset to 0:w
-//  OCR1A = 100;            // compare match register 16MHz/256/2Hz
-//  TCCR1B |= (1 << WGM12);   // CTC mode
-//  TCCR1B |= (1 << CS12);    // 256 prescaler 
-//  TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
-
+  PCICR |= (1 << PCIE1);      // enable pin change interrupt for PCINT14..8
+  PCMSK1 |= (1 << PCINT8);  // Enable Pin Change interrupt for ADC0 (analog PIN 0) INT8
+  IRQ8PrevTime = millis();    // Hold current time
 
   bitSet(TCCR1B ,CS12);  // Counter Clock source is external pin
   bitSet(TCCR1B ,CS11);  // Clock on rising edge  
   TCCR1A=0;        // reset timer/counter control register A
   interrupts();  
-  
-  pinMode(GreenLED,OUTPUT);
-  pinMode(RedLED,OUTPUT);
-  
-  Serial.begin(115200);
-  Serial.println("Pulse Counter");
-  Serial.println("Hours,  Counts/hour, total counts since restart");
+
+  LiquidCrystal lcd( 8, 9, 4, 5, 6, 7 );
+  lcd.begin(16, 2);
+  lcd.setCursor( 0, 0 );
+  lcd.print("Welcome");
+
+
 }  
 
-//called when timer reaches the compare match setting
-//won't happen as long as it's being set to 0 every second in the main loop
-ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
-{
-   Serial.println("Compare match");
+ISR(PCINT1_vect) {
+  int curVal;
+
+  long unsigned IRQ8ActTime = millis();
+
+  if (IRQ8ActTime - IRQ8PrevTime > 1) {
+    ADMUX = (analog_reference << 6) | (sensPin & 0x07);  // ADMUX - ADC Mulitplexer Selection Register
+    ADCSRA |= (1<<ADSC);                                 // ADCSRA: Bit 6 – ADC Start Conversion
+    while (bit_is_set(ADCSRA, ADSC));                    // While conversion in progress loop
+    uint8_t low = ADCL;                                  // read low value
+    uint8_t high = ADCH;                                 // read high value
+    curVal = (high << 8) | low;                          // combine low + high
+
+    if (curVal != prevVal) {
+      IRQ8PrevTime = IRQ8ActTime;
+      prevVal = curVal;
+      irqFlag = 1;
+    }
+    if(prevVal > 250) {
+      count++;
+    }
+  }
 }
 
-
-// every 65536 counts
-//won't happen as long as it's being set to 0 in the loop() every second
-
-ISR(TIMER1_OVF_vect)          
-{
-   Serial.println("Timer Overflow");
-}
 
 void loop()
 {
-  unsigned long start;
-  unsigned long elapsed;
-  unsigned int count;
-  unsigned long total=0L;
-  unsigned long counts_per_hour;
-  float hours;
+  unsigned long minutes;
+  unsigned long rate;
   const unsigned long millis_per_hour = (1000*60*60);
-  start = millis();
-  
-  for(;;) {
-  // start the counting
-   delay(samplePeriod);
-  
-  count = TCNT1;
-  TCNT1 = 0;
 
-  total += count;
+  minutes = millis() / 60000.0;
 
-  if(count != 0){
-    digitalWrite(RedLED, HIGH);
-    digitalWrite(GreenLED, LOW);
-  }else{
-    digitalWrite(GreenLED, HIGH);
-    digitalWrite(RedLED, LOW);
-  }
-  
-  elapsed = millis()-start;
-  
-  hours = ((float)(millis()-start))/(3600000.0F);//decimal hours since start
-  counts_per_hour = total/hours;
-  
-  Serial.print(hours);
-  Serial.print(",");
-  Serial.print(counts_per_hour);
-  Serial.print(",");
-  Serial.println(total);
-  
+  if((minutes != lastminutes) | (count != lastcount) ){
+    lastcount=count;
+    lastminutes = minutes;
+    if(count > 0) {
+      if(minutes>0) {
+        rate = count/minutes;
+      }
+      else{
+        rate=0;
+      }
+    }
+    else {
+      rate=0;
+    }
+    lcd.setCursor( 0, 0 );
+    lcd.print("                    ");  
+    lcd.setCursor( 0, 0 );
+    lcd.print((long) minutes);
+    lcd.setCursor(5,0);
+    lcd.print(rate);
+    lcd.setCursor(10,0);
+    lcd.print(count);
   }
 }
+
 
 
